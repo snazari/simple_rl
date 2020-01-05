335ac9b4f8eaa085f9650356a58ea03eb505f0df
Ubuntu <ubuntu@ip-172-31-14-157.us-east-2.compute.internal>
2020-01-05 00:47:50 +0000

    Fixed dict() issue

--- simple_rl/tasks/rock_paper_scissors/RockPaperScissorsMDPClass.py
+++ simple_rl/tasks/rock_paper_scissors/RockPaperScissorsMDPClass.py
@@ -25,7 +25,7 @@ class RockPaperScissorsMDP(MarkovGameMDP):
         Returns
             (float)
         '''
-        agent_a, agent_b = action_dict.keys()[0], action_dict.keys()[1]
+        agent_a, agent_b = list(action_dict.keys())[0], list(action_dict.keys())[1]
         action_a, action_b = action_dict[agent_a], action_dict[agent_b]
 
         reward_dict = {}
